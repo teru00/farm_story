6e337c7d70d17fafc0db4e24869d5a394c91da64
terufumi shimoji <e125761@ie.u-ryukyu.ac.jp>
2016-05-31 08:40:16 +0000

    カートとログインを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Farm Story"
+  end
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title","About | Farm Story"
+    assert_select "title", "About | Farm Story"
   end
 end
