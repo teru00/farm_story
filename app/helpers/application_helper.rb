69e8c2191ccf8355485c97f28d4aa53fddcf84fc
terufumi shimoji <e125761@ie.u-ryukyu.ac.jp>
2016-05-31 01:27:20 +0000

    Titleをちょっとだけ動的処理

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def provide_title(page_title = '')
+    base_title = "Farm Story"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title","About | Farm Story"
+  end
 end
