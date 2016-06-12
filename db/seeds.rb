9bedd3e0506946f9fcd3b9fcd417b423e418a1fe
terufumi shimoji <e125761@ie.u-ryukyu.ac.jp>
2016-06-12 01:16:52 +0000

    レイアウトが複雑過ぎたのでシンプルにした。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
 
-#User.create!(name: "shimoji terufumi",
-#             email: "e125761@ie.u-ryukyu.ac.jp",
-#             password: "foobar",
-#             password_confirmation: "foobar",
-#             admin: true)
-#
-#99.times do |n|
-#  name = Faker::Name.name
-#  email = "example-#{n+1}@railstutorial.org"
-#  password = "password"
-#  User.create!(name: name,
-#               email: email,
-#              password_confirmation: password)
-#end
+User.create!(name: "shimoji terufumi",
+             email: "e125761@ie.u-ryukyu.ac.jp",
+             password: "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
 
 30.times do |n|
   name = "Vegetable-#{n+1}"
