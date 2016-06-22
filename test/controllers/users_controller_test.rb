6dbad77baee82ff820b361f991502911c67cffe1
terufumi shimoji <e125761@ie.u-ryukyu.ac.jp>
2016-06-22 04:25:42 +0000

    controllerの単体テスト追記

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,26 +4,37 @@ class UsersControllerTest < ActionController::TestCase
   
   def setup
     @user = users(:terufumi)
-    @user = users(:akihisa)
+    @other_user = users(:akihisa)
   end
   
   test "should get new" do
     get :new
     assert_response :success
   end
-
+  
+  test "should get index when no logged in" do
+    get :index
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should get index when logged in" do
+    log_in_as(@user)
+    get :index
+    assert_response :success
+  end
+  
+  test "should get show" do
+    get :show, id: @user.id
+    assert_response :success
+  end
+  
   test "should redirect edit when not logged in" do
     get :edit, id: @user
     assert_not flash.empty?
     assert_redirected_to login_url
   end
   
-  test "should redirect update when not logged in" do
-    patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get :edit, id: @user
@@ -31,12 +42,6 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
